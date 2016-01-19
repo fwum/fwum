65715435b19e56fa962ad7f2b335082d9a636ec9
ryanisaacg <ryanisaacg@gmail.com>
2016-01-19 12:29:04 -0500

    Added more functions to slice

--- src/slice.c
+++ src/slice.c
@@ -1,10 +1,34 @@
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include "slice.h"
+
+slice new_slice(char* string)
+{
+	return make_slice(string, 0, strlen(string));
+}
+
+slice make_slice(char* string, int begin, int end)
+{
+	slice s;
+	s.data = string;
+	s.begin = begin;
+	s.end = end;
+	return s;
+}
 
 bool equals(slice s1, slice s2)
 {
 	return s1.data == s2.data && s1.begin == s2.begin && s1.end == s2.end;
+}
+
+bool starts_with(slice s1, slice s2)
+{
+	if(s1.end - s1.begin < s2.end - s2.begin) return false;
+	for(int i = 0; i < s2.end - s2.end; i++)
+		if(s1.data[s1.begin + i] != s2.data[s2.begin + i])
+			return false;
+	return true;
 }
 
 bool equals_string(slice s1, char* data)
--- src/slice.h
+++ src/slice.h
@@ -5,7 +5,10 @@ typedef struct slice
 	char* data;
 	int begin, end;
 } slice;
+slice new_slice(char* string);
+slice make_slice(char* string, int start, int end);
 bool equals(slice s1, slice s2);
 bool equals_string(slice s1, char* data);
+bool starts_with(slice s1, slice s2);
 char* evaluate(slice s1);
 #endif
