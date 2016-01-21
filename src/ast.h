9f32e5fe6b1a6917409fd3609928b30e09e2f390
Ryan Goldstein <ryanisaacg@gmail.com>
2016-01-21 06:27:41 -0500

    Added functions for identifying literals

--- src/ast.h
+++ src/ast.h
@@ -2,7 +2,7 @@
 #define AST_H_
 typedef enum ast_type
 {
-	ROOT, IMPORT, USING, VARTYPE, STRUCT, TYPE, FUNC, BIND, TYPED_BIND, ASSIGN
+	ROOT, IMPORT, USING, VARTYPE, STRUCT, TYPE, FUNC, BIND, TYPED_BIND, ASSIGN, STRING, NUMBER, VAR
 } ast_type;
 typedef struct ast_node
 {
@@ -16,4 +16,7 @@ char* to_string(ast_node *root);
 char* type_to_string(ast_type type);
 int length_list(ast_node *start);
 void add_child(ast_node *root, ast_node *child);
+bool is_number_literal(slice literal);
+bool is_string_literal(slice literal);
+bool is_identifier_literal(slice literal);
 #endif
