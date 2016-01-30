e21df34dcb40c6088019c71d9113e05e241b5c81
Ryan Goldstein <ryanisaacg@gmail.com>
2016-01-30 10:52:26 -0500

    Demonstrated the beginning of the C backend

--- src/main.c
+++ src/main.c
@@ -1,10 +1,11 @@
 #include "parser.h"
 #include "ast.h"
+#include "cbackend.h"
 #include <stdio.h>
 
 int main()
 {
-	ast_node *root = parse("import x; using a; c getC(a : b) { if((a == b)) { let value = 2.1 + (call(param) + 3); } } struct c{a:b;} import f;");
-	printf("%s\n", to_string(root));
+	ast_node *root = parse("import x; using a; c getC(a : b) { if((a == b)) { let value = 2.1 + (call(param) + 3); } } struct c{a : int;} import f;");
+	compile(root, stdout);
 	return 0;
 }
