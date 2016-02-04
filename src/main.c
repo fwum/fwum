0112114d3a97fe1daceebb26ba5e39919bbd0fce
Ryan Goldstein <ryanisaacg@gmail.com>
2016-02-04 16:26:58 -0500

    Compiled from actual files instead of from hard-coded input

--- src/main.c
+++ src/main.c
@@ -1,11 +1,23 @@
 #include "parser.h"
 #include "ast.h"
 #include "cbackend.h"
+#include "io.h"
 #include <stdio.h>
 
-int main()
+int main(int argc, char **argv)
 {
-	ast_node *root = parse("import x; using a; c getC(a : b) { if((a == b)) { let value : c = 2.1 + (call(param) + 3); } } struct c{a : int;} import f;");
-	compile(root, stdout);
-	return 0;
+	if(argc <= 1)
+	{
+		printf("No input files.\n");
+		return -1;
+	}
+	else
+	{
+		FILE* input = fopen(argv[1], "r");
+		char* data = read_file(input);
+		fclose(input);
+		ast_node *root = parse(data);
+		compile(root, stdout);
+		return 0;
+	}
 }
