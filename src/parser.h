99306622f06ac9ff1c5fe2339c5afedd002311db
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-03 21:49:29 -0500

    Ripped apart compiler to create simpler parser

--- src/parser.h
+++ src/parser.h
@@ -1,5 +1,19 @@
 #ifndef PARSER_H_
 #define PARSER_H_
-#include "ast.h"
-ast_node* parse(char* data, char *filename);
+#include "slice.h"
+struct parse_token;
+struct token_list;
+typedef struct parse_token parse_token;
+typedef struct token_list token_list;
+struct parse_token {
+    slice data;
+    enum {OPERATOR, WORD, NUMBER, BLOCK_START, BLOCK_END, STRING, CHARACTER} type;
+    char *originFile;
+    int line;
+    parse_token *next;
+};
+struct token_list {
+    parse_token *head, *tail;
+};
+parse_token *parse(char *data, char *filename);
 #endif
