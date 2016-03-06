d11448eeecb92cdb2c39e4b463805f347a0922fe
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-06 07:59:05 -0500

    Moved the type enum from an anonymous enum to a real type

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -3,11 +3,13 @@
 #include "slice.h"
 struct parse_token;
 struct token_list;
+enum token_type;
 typedef struct parse_token parse_token;
 typedef struct token_list token_list;
+typedef enum token_type token_type;
 struct parse_token {
     slice data;
-    enum {OPERATOR, WORD, NUMBER, START, END, STRING_LIT, CHAR_LIT} type;
+    token_type type;
     char *originFile;
     int line;
     parse_token *next;
@@ -15,5 +17,8 @@ struct parse_token {
 struct token_list {
     parse_token *head, *tail;
 };
+enum token_type {
+    OPERATOR, WORD, NUMBER, START, END, STRING_LIT, CHAR_LIT;
+}
 parse_token *parse(char *data, char *filename);
 #endif
