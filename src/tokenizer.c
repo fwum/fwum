b03a72d8cdd1bca106a975bbe294aabeeade80bd
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-06 08:23:16 -0500

    Added functions to tell if a character is an alphabetical character or a number

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,4 +1,5 @@
 #include "parser.h"
+#include "slice.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
@@ -9,10 +10,15 @@ SYMBOL, WORD, NUMBER, START, END, STRING_LIT, CHAR_LIT
 token_list parse(char *data, char *filename)
 {
     token_list list = {NULL, NULL};
-    slice current = make_slice(data, 0);
     int length = strlen(data);
+    int token_begin = 0, token_end = 0;
+    int source_line = 0;
+    token_type mode;
     for(int i = 0; i < length; i++)
     {
+        //Ingore carriage return characters so that windows newlines are identical to UNIX newlines
+        if(data[i] == '\r') continue;
+        if(dtaa[i] == '\n') source_line += 1;
 
     }
     return list;
@@ -49,4 +55,14 @@ static void token_add(token_list list, parse_token token)
 static bool is_whitespace(char c)
 {
     return c == ' ' || c == '\t' || c == '\n';
+}
+
+static bool is_alpha(char c)
+{
+    return (c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z');
+}
+
+static bool is_num(char c)
+{
+    return c >= '0' && c <= '9';
 }
