a91f3d592dc8ad1ab1a54c159719d373b6918343
ryanisaacg <ryanisaacg@gmail.com>
2016-03-07 13:00:27 -0500

    Removed trailing semicolon from the DEFSTRUCT macro

--- src/util.h
+++ src/util.h
@@ -1,6 +1,6 @@
 #ifndef UTIL_H_
 #define UTIL_H_
-#define DEFSTRUCT(name) struct name; typedef struct name name;
+#define DEFSTRUCT(name) struct name; typedef struct name name
 #define new(name) malloc(sizeof(*name))
 #define array(name, len) malloc(sizeof(*name) * len)
 #endif
