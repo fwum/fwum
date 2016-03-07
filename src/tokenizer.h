fcaae59911b3e9c2697d23991b53e13879ab279c
ryanisaacg <ryanisaacg@gmail.com>
2016-03-07 13:00:56 -0500

    Created an origin struct rather than storing the filename and the line number directly

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -1,23 +1,27 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 #include "slice.h"
-struct parse_token;
-struct token_list;
-typedef struct parse_token parse_token;
-typedef struct token_list token_list;
+#include "util.h"
+DEFSTRUCT(parse_token);
+DEFSTRUCT(token_list);
+DEFSTRUCT(source_origin);
+struct source_origin {
+    char *filename;
+    int line;
+};
 typedef enum token_type {
     SYMBOL, WORD, NUMBER, STRING_LIT, CHAR_LIT
 } token_type;
 struct parse_token {
     slice data;
     token_type type;
-    char *originFile;
-    int line;
+    source_origin origin;
     parse_token *next;
 };
 struct token_list {
     parse_token *head, *tail;
 };
+
 token_list parse(char *data, char *filename);
 void print_tlist(token_list list);
 #endif
