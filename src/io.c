4452c4a7f4e46521667cc3da48133914dc2611e7
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-15 18:34:54 -0400

    Added documentation to the IO module

--- src/io.c
+++ src/io.c
@@ -4,14 +4,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+/*
+Reads a file in its entirety
+Intended to be immune to buffer overflows
+Will hang forever if given stdin, for example, so don't do that
+*/
 char* read_file(FILE* stream)
 {
 	char* contents = new(contents);
 	int spot = 0, length = 0;
-	const int CHUNK = 1024;
+	const int CHUNK = 1024; //size of the chunk to read at a time
 	char current = fgetc(stream);
 	while(current != EOF)
 	{
+		//Out of space in the array
 		if(spot == length)
 		{
 			contents = realloc(contents, length + CHUNK);
@@ -21,6 +27,6 @@ char* read_file(FILE* stream)
 		current = fgetc(stream);
 		spot += 1;
 	}
-	contents[spot + 1] = '\0';
+	contents[spot + 1] = '\0'; //Ensure the string is null-terminated
 	return contents;
 }
