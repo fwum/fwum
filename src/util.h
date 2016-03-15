fce274209f08b621b1856c600f77f9da0178387e
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-15 16:34:54 -0400

    Added parentheses around macro operands so they are expanded correctly

--- src/util.h
+++ src/util.h
@@ -3,7 +3,7 @@
 //Forward declare a struct
 #define DEFSTRUCT(name) struct name; typedef struct name name
 //Allocates memory for a variable on the heap
-#define new(name) malloc(sizeof(*name))
+#define new(name) malloc(sizeof(*(name)))
 //Creates an array of a certain size on the heap
-#define array(name, len) malloc(sizeof(*name) * len)
+#define array(name, len) malloc(sizeof(*(name)) * len)
 #endif
