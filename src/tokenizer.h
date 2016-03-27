3383d8956f347dcf1e11a6f098fdad5e6c35e08f
Ryan Goldstein <ryanisaacg@gmail.com>
2016-03-27 18:09:31 -0400

    Documented the tokenizer header file

--- src/tokenizer.h
+++ src/tokenizer.h
@@ -12,6 +12,10 @@ struct source_origin {
 typedef enum token_type {
     SYMBOL, WORD, NUMBER, STRING_LIT, CHAR_LIT
 } token_type;
+/*
+A token from a source filename
+The result from the direct-text read and the operand for all future steps in compilation
+*/
 struct parse_token {
     slice data;
     token_type type;
@@ -22,6 +26,12 @@ struct token_list {
     parse_token *head, *tail;
 };
 
+/*
+Convert the text data from the file filename into a list of tokens
+*/
 token_list parse(char *data, char *filename);
+/*
+Print a list of tokens to stdout, mostly for debugging purposes
+*/
 void print_tlist(token_list list);
 #endif
