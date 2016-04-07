8e67e696976c862b641ce34c155d32efd193634c
ryanisaacg <ryanisaacg@gmail.com>
2016-04-06 20:21:12 -0400

    Associated each operator with its expression type to simplify parsing

--- src/operators.c
+++ src/operators.c
@@ -1,56 +1,59 @@
 #include "operators.h"
+#include "semantic_analyzer.h"
+#include "util.h"
 #include <stdlib.h>
-static operator_node *new_operator_node(char *data);
-static void add_next(operator_node *current, char *data);
-static operator_node* set_child(operator_node *current, char *data);
+static operator_node *new_operator_node(char *data, statement_type type);
+static void add_next(operator_node *current, char *data, statement_type type);
+static operator_node* set_child(operator_node *current, char *data, statement_type type);
 
-static operator_node *new_operator_node(char *data)
+static operator_node *new_operator_node(char *data, statement_type type)
 {
-	operator_node *newNode = malloc(sizeof(*newNode));
+	operator_node *newNode = new(newNode);
 	newNode->data = data;
 	newNode->next = NULL;
 	newNode->child = NULL;
+	newNode->operatorType = type;
 	return newNode;
 }
 
-static void add_next(operator_node *current, char *data)
+static void add_next(operator_node *current, char *data, statement_type type)
 {
-	operator_node *newNode = new_operator_node(data);
+	operator_node *newNode = new_operator_node(data, type);
 	newNode->next = current->next;
 	current->next = newNode;
 }
 
-static operator_node* set_child(operator_node *current, char *data)
+static operator_node* set_child(operator_node *current, char *data, statement_type type)
 {
-	operator_node *newNode = new_operator_node(data);
+	operator_node *newNode = new_operator_node(data, type);
 	current->child = newNode;
 	return newNode;
 }
 
 operator_node* get_node()
 {
-	operator_node *root = new_operator_node("=");
-	operator_node *current = set_child(root, "||");
-	add_next(current, "|");
-	current = set_child(current, "^^");
-	add_next(current, "^");
-	current = set_child(current, "&&");
-	add_next(current, "&");
-	current = set_child(current, "!=");
-	add_next(current, "==");
-	add_next(current, "<=");
-	add_next(current, ">=");
-	add_next(current, "<");
-	add_next(current, ">");
-	current = set_child(current, ">>");
-	add_next(current, ">>>");
-	add_next(current, "<<");
-	current = set_child(current, "+");
-	add_next(current, "-");
-	add_next(current, "%");
-	current = set_child(current, "*");
-	add_next(current, "/");
-	current = set_child(current, "**");
-	current = set_child(current, ".");
+	operator_node *root = new_operator_node("=", OP_ASSGIN);
+	operator_node *current = set_child(root, "||", OP_BOOL_OR);
+	add_next(current, "|", OP_BIT_OR);
+	current = set_child(current, "^^", OP_BOOL_XOR);
+	add_next(current, "^", OP_BIT_XOR);
+	current = set_child(current, "&&", OP_BOOL_AND);
+	add_next(current, "&", OP_BIT_AND);
+	current = set_child(current, "!=", OP_NOT_EQUAL);
+	add_next(current, "==", OP_EQUAL);
+	add_next(current, "<=", OP_LESS_EQUAL);
+	add_next(current, ">=", OP_GREATER_EQUAL);
+	add_next(current, "<", OP_LESS);
+	add_next(current, ">", OP_GREATER);
+	current = set_child(current, ">>", OP_SHIFT_RIGHT);
+	add_next(current, ">>>", OP_BIT_SHIFT_RIGHT);
+	add_next(current, "<<", OP_SHIFT_LEFT);
+	current = set_child(current, "+", OP_ADD);
+	add_next(current, "-", OP_SUB);
+	add_next(current, "%", OP_MOD);
+	current = set_child(current, "*", OP_MULT);
+	add_next(current, "/", OP_DIV);
+	current = set_child(current, "**", OP_EXP);
+	current = set_child(current, ".", OP_MEMBER);
 	return root;
 }
--- src/operators.h
+++ src/operators.h
@@ -1,5 +1,7 @@
 #ifndef OPERATOR_H_
 #define OPERATOR_H_
+#include "semantic_analyzer.h"
+
 struct operator_node;
 typedef struct operator_node operator_node;
 
@@ -7,6 +9,7 @@ struct operator_node {
 	char *data;
 	operator_node *next;
 	operator_node *child;
+	statement_type operatorType;
 };
 
 operator_node* get_node();
