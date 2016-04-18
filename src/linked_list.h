21d374818b21b43274d15d83fa177885f45b0c99
ryanisaacg <ryanisaacg@gmail.com>
2016-04-17 20:02:24 -0400

    Added a linked iterator structure and the function declarations for it

--- src/linked_list.h
+++ src/linked_list.h
@@ -3,6 +3,8 @@
 #include "util.h"
 DEFSTRUCT(linked_list);
 DEFSTRUCT(linked_node);
+DEFSTRUCT(linked_iter);
+
 struct linked_node {
 	void *data;
 	linked_node *next, *prev;
@@ -10,8 +12,22 @@ struct linked_node {
 struct linked_list {
 	linked_node *head, *tail;
 };
+struct linked_iter {
+	linked_node *current;
+}
+
 void ll_add_first(linked_list *list, void *value);
 void ll_add_last(linked_list *list, void *value);
 void *ll_remove_last(linked_list *list);
 void *ll_remove_first(linked_list *list);
+
+linked_iter ll_iter_head(linked_list *list);
+linked_iter ll_iter_tail(linked_list *list);
+void *ll_iter_next(linked_iter *iter);
+void *ll_iter_prev(linked_iter *iter);
+bool ll_iter_has_next(linked_iter *iter);
+bool ll_iter_has_prev(linked_iter *iter);
+void ll_iter_add_after(linked_iter *iter, void *data);
+void ll_iter_add_before(linked_iter *iter, void *data);
+void ll_iter_remove(linked_iter *iter);
 #endif
