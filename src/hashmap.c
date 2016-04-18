e03a124a541b70a6a4c37b3231022c26f276f81b
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-17 20:38:55 -0400

    Fixed accidentally trying to index the hashmap instead of its member of arrays

--- src/hashmap.c
+++ src/hashmap.c
@@ -9,20 +9,20 @@ hash_map *hm_new()
 {
 	hash_map *map = new(map);
 	for(int i = 0; i < HASHMAP_ENTRY_LENGTH; i++)
-		map[i] = {NULL, NULL};
+		map->entries[i].head = map->entries[i].tail = NULL;
 	return map;
 }
 
 void hm_put(hash_map *map, int key, void *value)
 {
 	int index = hash(key);
-	ll_add_last(map[index], value);
+	ll_add_last(&(map->entries[index]), value);
 }
 
 void *hm_get(hash_map *map, int key)
 {
 	int index = hash(key);
-	linked_list data = map[index];
+	linked_list data = map->entries[index];
 	//TODO: Use an iterator to find the key
 	return NULL;
 }
@@ -33,7 +33,7 @@ bool hm_has(hash_map *map, int key)
 	return false;
 }
 
-static int hash(int key) 
+static int hash(int key)
 {
 	return key % HASHMAP_ENTRY_LENGTH;
 }
