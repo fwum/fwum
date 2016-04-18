29664bed961330ea320b64d7bfb1946879b1559b
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-18 06:33:51 -0400

    Implemented hashmap functions

--- src/hashmap.c
+++ src/hashmap.c
@@ -16,21 +16,29 @@ hash_map *hm_new()
 void hm_put(hash_map *map, int key, void *value)
 {
 	int index = hash(key);
-	ll_add_last(&(map->entries[index]), value);
+	hash_entry *entry = new(entry);
+	entry->key = key;
+	entry->value = value;
+	ll_add_last(&(map->entries[index]), entry);
 }
 
 void *hm_get(hash_map *map, int key)
 {
 	int index = hash(key);
 	linked_list data = map->entries[index];
-	//TODO: Use an iterator to find the key
+	linked_iter iterator = ll_iter_head(&data);
+	while(ll_iter_has_next(&iterator))
+	{
+		hash_entry *entry = ll_iter_next(&iterator);
+		if(entry->key == key)
+			return entry->value;
+	}
 	return NULL;
 }
 
 bool hm_has(hash_map *map, int key)
 {
-	//TODO: use an iterator to find the key
-	return false;
+	return hm_get(map, key) != NULL;
 }
 
 static int hash(int key)
