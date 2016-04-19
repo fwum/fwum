a709d576d8a937103da9d4ab9f3c21889c88a10a
ryanisaacg <ryanisaacg@gmail.com>
2016-04-19 09:32:34 -0400

    Implemented a test set system, where tests are grouped by what they are testing

--- src/main.c
+++ src/main.c
@@ -45,9 +45,20 @@ int main(int argc, char **argv)
 #ifdef DO_TESTS
 #include "linked_list.h"
 #include "stdio.h"
-#define ASSERT(value, test_name) do { printf("Test:\t%s\t", (test_name)); \
-	if(!(value)) { printf("FAILED\n"); exit(-1); } else { printf("PASSED\n"); } } \
+DEFSTRUCT(test_set);
+struct test_set {
+	char *name;
+	int passed, failed;
+};
+
+#define DO_TEST_SET(input_name) do {char *name = (input_name); printf("Test set: %s\n", name); current_test.name = name; current_test.passed = 0; current_test.failed = 0; } while(0);
+#define END_TEST_SET() printf("%s finished, with %d tests passing and %d tests failing.\n**********************\n", current_test.name, current_test.passed, current_test.failed);
+
+#define ASSERT(value, test_name) do { if(!(value)) { printf("FAILED"); current_test.failed += 1; } else { printf("PASSED"); current_test.passed += 1;} \
+	printf(":\t%s\n", test_name);} \
 	while(0);
+
+static test_set current_test;
 
 static int *box_int(int value)
 {
@@ -58,7 +69,10 @@ static int *box_int(int value)
 
 static int tests()
 {
+	DO_TEST_SET("Test Framework");
 	ASSERT(true, "Test framework works");
+	END_TEST_SET()
+	DO_TEST_SET("Linked List");
 	linked_list list = {NULL, NULL};
 	ASSERT(ll_empty(&list), "Linked list empty function");
 	ll_add_first(&list, box_int(5));
@@ -67,6 +81,7 @@ static int tests()
 	ASSERT(*((int*)ll_iter_next(&iterator)) == 6, "Linked list iterator");
 	ll_clear(&list);
 	ASSERT(ll_empty(&list), "Linked list clear function");
+	END_TEST_SET();
 	return 0;
 }
 #endif
