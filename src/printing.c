06b57be0e8c4d4448c3881921e5f336d5dcbabf0
Ryan Goldstein <ryanisaacg@gmail.com>
2016-04-23 11:21:11 -0400

    Updated the printing module to the newest bracket style

--- src/printing.c
+++ src/printing.c
@@ -10,23 +10,20 @@ static void print_token(parse_token *current);
 /*
 Print a list of tokens to stdout, mostly for debugging purposes
 */
-void print_tlist(token_list list)
-{
-    if(list.head == NULL)
+void print_tlist(token_list list) {
+    if(list.head == NULL) {
         return;
+    }
     parse_token *current = list.head;
-    while(current != list.tail)
-    {
+    while(current != list.tail) {
         print_token(current);
         current = current->next;
     }
     print_token(current);
 }
 
-static void print_token(parse_token *current)
-{
-    switch(current->type)
-    {
+static void print_token(parse_token *current) {
+    switch(current->type) {
     case SYMBOL:
         printf("SYMBOL: %s\n", evaluate(current->data));
         break;
@@ -48,16 +45,13 @@ static void print_token(parse_token *current)
 /*
 Print the created AST to stdout, mostly for debugging purposes
 */
-void dump(file_contents contents)
-{
+void dump(file_contents contents) {
 	{
 		struct_declaration *current = contents.head;
-		while(current != NULL)
-		{
+		while(current != NULL) {
 			printf("STRUCT: %s\n", evaluate(current->name));
 			struct_member *member = current->head;
-			while(member != NULL)
-			{
+			while(member != NULL) {
 				printf("\tMEMBER: NAME: %s | TYPE: %s\n", evaluate(member->name), evaluate(member->type));
 				member = member->next;
 			}
@@ -66,8 +60,7 @@ void dump(file_contents contents)
 	}
 	{
 		func_declaration *current = contents.funcHead;
-		while(current != NULL)
-		{
+		while(current != NULL) {
 			printf("FUNC: %s | TYPE : %s\n", evaluate(current->name), evaluate(current->type));
 			dump_node(current->paramHead, 0);
 			dump_node(current->root, 0);
@@ -80,14 +73,14 @@ void dump(file_contents contents)
 /*
 Print an AST node to stdout (recursive)
 */
-void dump_node(statement *state, int indentation)
-{
-	if(state == NULL)
+void dump_node(statement *state, int indentation) {
+	if(state == NULL) {
 		return;
-	for(int i = 0; i < indentation; i++)
+    }
+	for(int i = 0; i < indentation; i++) {
 		printf("\t");
-	switch(state->type)
-	{
+    }
+	switch(state->type) {
         case OP_ADD:
         	printf("OP_ADD");
         break;
@@ -231,8 +224,10 @@ void dump_node(statement *state, int indentation)
         break;
 	}
 	printf(": %s\n", evaluate(state->data));
-	if(state->child != NULL)
+	if(state->child != NULL) {
 		dump_node(state->child, indentation + 1);
-	if(state->next != NULL)
+    }
+	if(state->next != NULL) {
         dump_node(state->next, indentation);
+    }
 }
