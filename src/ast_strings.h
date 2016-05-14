e1b1f5ccd46968796fb2e63c05cd1ffc0748959a
Ryan Goldstein <ryanisaacg@gmail.com>
2016-05-14 09:37:05 -0400

    Added documentation to the ast_string header

--- src/ast_strings.h
+++ src/ast_strings.h
@@ -2,6 +2,8 @@
 #define AST_STRINGS_H_
 #include "slice.h"
 #include "semantic_analyzer.h"
+//Parse a statement type from a string
 statement_type statement_from_slice(slice string);
+//Convert a statement type to a string
 char *statement_to_string(statement_type type);
 #endif
