943920b2683b9a61e30e93ac83a364e5663075f9
ryanisaacg <ryanisaacg@gmail.com>
2016-05-16 14:00:46 -0400

    Fixed union access in the type module

--- src/types.c
+++ src/types.c
@@ -7,28 +7,29 @@ type get_type(file_contents context, slice type_descriptor) {
     type t;
     if((first == 'u' || first == 'i' || first == 'f') && is_numeric(amt)) {
         t.is_numeric = true;
-        int size = parse_int(amt);
-        t.primitive.bits = size;
+        optional op = parse_int(amt);
+		int size = *((int*)op_get(op));
+        t.data.numeric.bits = size;
         switch(first) {
         case 'u':
-            t.primitive.type = UNSIGNED;
+            t.data.numeric.type = UNSIGNED;
             break;
         case 'i':
-            t.primitive.type = SIGNED;
+            t.data.numeric.type = SIGNED;
             break;
         case 'f':
-            t.primitive.type = FLOAT;
+            t.data.numeric.type = FLOAT;
             break;
         }
     } else {
         t.is_numeric = false;
-        t.declared = NULL;
+        t.data.declared = NULL;
         linked_list *structs = context.structs;
         linked_iter iterator = ll_iter_head(structs);
         while(ll_iter_has_next(&iterator)) {
             struct_declaration *dec = ll_iter_next(&iterator);
             if(equals(type_descriptor, dec->name))
-                t.declared = dec;
+                t.data.declared = dec;
         }
     }
     return t;
