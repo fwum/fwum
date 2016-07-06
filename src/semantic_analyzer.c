09e50dbaa60a2fc2780bd5a39950b3ec5585a662
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-06 08:56:20 -0400

    Implemented enums in the semantic analyzer

--- src/semantic_analyzer.c
+++ src/semantic_analyzer.c
@@ -1,8 +1,10 @@
+#include <math.h>
 #include "semantic_analyzer.h"
 #include "parser.h"
 #include "symbol_table.h"
 #include "types.h"
 #include "util.h"
+#include "linked_list.h"
 
 static type get_node_type(symbol_table *context, statement *expr);
 static void analyze_node(symbol_table *context, statement *node);
@@ -10,8 +12,20 @@ static type leftmost_type(symbol_table *context, statement *expr);
 static type rightmost_type(symbol_table *context, statement *expr);
 
 void analyze(file_contents contents) {
-    linked_iter iterator = ll_iter_head(contents.functions);
     symbol_table *toplevelSymbols = st_new();
+    //Analyze function return types
+    linked_iter iterator = ll_iter_head(contents.functions);
+    while(ll_iter_has_next(&iterator)) {
+        func_declaration *dec = ll_iter_next(&iterator);
+        type returnType = get_type(toplevelSymbols, dec->type);
+        type *boxedType = new(boxedType);
+        *boxedType = returnType;
+        slice *boxedSlice = new(boxedSlice);
+        *boxedSlice = dec->name;
+        st_put(toplevelSymbols, boxedSlice, boxedType);
+        dec->type = type_to_string(returnType);
+    }
+    //Analyze struct and struct members
     iterator = ll_iter_head(contents.structs);
     while(ll_iter_has_next(&iterator)) {
         struct_declaration *dec = ll_iter_next(&iterator);
@@ -24,15 +38,17 @@ void analyze(file_contents contents) {
         *boxedSlice = dec->name;
         st_put(toplevelSymbols, boxedSlice, boxedType);
     }
+    iterator = ll_iter_head(contents.enums);
     while(ll_iter_has_next(&iterator)) {
-        func_declaration *dec = ll_iter_next(&iterator);
-        type returnType = get_type(toplevelSymbols, dec->type);
+        enum_declaration *dec = ll_iter_next(&iterator);
+        type enumType;
+        enumType.kind = ENUM;
+        enumType.data.enum_dec = dec;
         type *boxedType = new(boxedType);
-        *boxedType = returnType;
+        *boxedType = enumType;
         slice *boxedSlice = new(boxedSlice);
         *boxedSlice = dec->name;
         st_put(toplevelSymbols, boxedSlice, boxedType);
-        dec->type = type_to_string(returnType);
     }
     iterator = ll_iter_head(contents.functions);
     while(ll_iter_has_next(&iterator)) {
@@ -55,78 +71,91 @@ void analyze(file_contents contents) {
 
 static void analyze_node(symbol_table *context, statement *node) {
     switch(node->type) {
-    case OP_INIT: {
-        statement *name = ll_get_first(node->children);
-        statement *value = ll_get_last(node->children);
-        type t = get_node_type(context, value);
-        type *boxed_type = new(boxed_type);
-        *boxed_type = t;
-        st_put(context, &(name->data), boxed_type);
-        name->children = ll_new();
-        statement *type_statement = new(type_statement);
-        type_statement->children = NULL;
-        type_statement->origin = name->origin;
-        type_statement->type = TYPE;
-        type_statement->data = type_to_string(t);
-        ll_add_last(name->children, type_statement);
-    } break;
-    default:
-        if(node->children != NULL) {
-            linked_iter children = ll_iter_head(node->children);
-            while(ll_iter_has_next(&children)) {
-                statement *child = ll_iter_next(&children);
-                analyze_node(context, child);
-            }
-        }
-        break;
+        case OP_INIT: {
+                          statement *name = ll_get_first(node->children);
+                          statement *value = ll_get_last(node->children);
+                          type t = get_node_type(context, value);
+                          type *boxed_type = new(boxed_type);
+                          *boxed_type = t;
+                          st_put(context, &(name->data), boxed_type);
+                          name->children = ll_new();
+                          statement *type_statement = new(type_statement);
+                          type_statement->children = NULL;
+                          type_statement->origin = name->origin;
+                          type_statement->type = TYPE;
+                          type_statement->data = type_to_string(t);
+                          ll_add_last(name->children, type_statement);
+                      } break;
+        default:
+                      if(node->children != NULL) {
+                          linked_iter children = ll_iter_head(node->children);
+                          while(ll_iter_has_next(&children)) {
+                              statement *child = ll_iter_next(&children);
+                              analyze_node(context, child);
+                          }
+                      }
+                      break;
     }
 }
 
 static type get_node_type(symbol_table *context, statement *expr) {
     switch(expr->type) {
-    case NAME:
-        return *st_get_type(context, &(expr->data));
-    case NUM:
-        if(slice_contains(expr->data, '.'))
-            return make_numeric_type(FLOAT, 32);
-        else
-            return make_numeric_type(SIGNED, 32);
-    case OP_ASSIGN:
-        return rightmost_type(context, expr);
-    case OP_BOOL_OR:
-    case OP_BOOL_XOR:
-    case OP_BOOL_AND:
-    case OP_EQUAL:
-    case OP_NOT_EQUAL:
-    case OP_GREATER:
-    case OP_LESS:
-    case OP_GREATER_EQUAL:
-    case OP_LESS_EQUAL:
-        return make_numeric_type(SIGNED, 8);
-    case OP_DEREF:
-       return dereference(rightmost_type(context, expr));
-    case OP_GETREF:
-       return reference(rightmost_type(context, expr));
-    case OP_INDEX:
-       return get_index(leftmost_type(context, expr));
-    case FUNC_CALL:
-       return *st_get_type(context, &(expr->data));
-    case HEAP_INIT:
-       return reference(*st_get_type(context, &((parse_token*)ll_get_first(expr->children))->data));
-    case OP_MEMBER: {
-        type left = leftmost_type(context, expr);
-        statement *right = ll_get_last(expr->children);
-        slice name = right->data;
-        //TODO: Errors if the dot operator is invalid
-        struct_declaration *dec = left.data.declared;
-        linked_iter iterator = ll_iter_head(dec->members);
-        while(ll_iter_has_next(&iterator)) {
-            struct_member *member = ll_iter_next(&iterator);
-            if(equals(member->name, name)) {
-                return get_type(context, member->type);
-            }
+        case NAME:
+            return *st_get_type(context, &(expr->data));
+        case NUM:
+            if(slice_contains(expr->data, '.'))
+                return make_numeric_type(FLOAT, 32);
+            else
+                return make_numeric_type(SIGNED, 32);
+        case OP_ASSIGN:
+            return rightmost_type(context, expr);
+        case OP_BOOL_OR:
+        case OP_BOOL_XOR:
+        case OP_BOOL_AND:
+        case OP_EQUAL:
+        case OP_NOT_EQUAL:
+        case OP_GREATER:
+        case OP_LESS:
+        case OP_GREATER_EQUAL:
+        case OP_LESS_EQUAL:
+            return make_numeric_type(SIGNED, 8);
+        case OP_DEREF:
+            return dereference(rightmost_type(context, expr));
+        case OP_GETREF:
+            return reference(rightmost_type(context, expr));
+        case OP_INDEX:
+            return get_index(leftmost_type(context, expr));
+        case FUNC_CALL:
+            return *st_get_type(context, &(expr->data));
+        case HEAP_INIT:
+            return reference(*st_get_type(context, &((parse_token*)ll_get_first(expr->children))->data));
+        case OP_MEMBER: {
+                            type left = leftmost_type(context, expr);
+                            statement *right = ll_get_last(expr->children);
+                            slice name = right->data;
+                            //TODO: Errors if the dot operator is invalid
+                            struct_declaration *dec = left.data.declared;
+                            linked_iter iterator = ll_iter_head(dec->members);
+                            while(ll_iter_has_next(&iterator)) {
+                                struct_member *member = ll_iter_next(&iterator);
+                                if(equals(member->name, name)) {
+                                    return get_type(context, member->type);
+                                }
+                            }
+                        }
+        case OP_NAMESPACE: {
+           type left = leftmost_type(context, expr);
+           if(left.kind == ENUM) {
+               int size = 8;
+               int enum_items = ll_size(left.data.enum_dec->options);
+               while(pow(2, size) < enum_items)
+                   size *= 2;
+               return make_numeric_type(UNSIGNED, size);
+           } else {
+               //TODO: handle namespaces
+               return left;
+           }
         }
-    }
 
     default: {
         type t;
