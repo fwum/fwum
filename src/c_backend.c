efbf104510a01435564d699ade8eaf02848b16f5
Ryan Goldstein <ryanisaacg@gmail.com>
2016-07-11 12:47:52 -0400

    The C backend now supports else

--- src/c_backend.c
+++ src/c_backend.c
@@ -76,7 +76,7 @@ static void output_container(struct_declaration *dec, char *type, FILE *stream) 
         fprintf(stream, "%s %s;\n", evaluate(mem->type), evaluate(mem->name));
     }
     fprintf(stream, "};\n");
-    
+
 }
 
 
@@ -241,10 +241,12 @@ static void output_node(statement *expr, FILE *stream) {
         fprintf(stream, "))");
     break;
     case IF:
-    	fprintf(stream, "if(");
-        output_node(ll_get_first(expr->children), stream);
+        iterator = ll_iter_head(expr->children);
+        fprintf(stream, "if(");
+        output_node(ll_iter_next(&iterator), stream);
         fprintf(stream, ")");
-        output_node(ll_get_last(expr->children), stream);
+        while(ll_iter_has_next(&iterator))
+            output_node(ll_iter_next(&iterator), stream);
         break;
 	case ELSE:
         fprintf(stream, "else");
